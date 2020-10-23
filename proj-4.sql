202a6da06a949e1639c6cb4df90be89d6f3675e6
kcolson8 <kelliepanda@gmail.com>
2020-10-22 22:25:42 -0700

    added feedback survey user

--- proj-4.sql
+++ proj-4.sql
@@ -105,7 +105,6 @@ INSERT INTO Destinations VALUES ("Bali", "Indonesia", true, true, true, false, t
                                 ("Sydney", "Australia", true, false, true, false, true, true, false, true, 4.3),
                                 ("Paris", "France", false, false, true, true, false, false, false, false, 3.9);
 
-<<<<<<< HEAD
 INSERT INTO User Values 
 ("gpautz", "rainbow!", "Ghar", "Pautz", "123 Ermina Ave", "12345"),
 ("kcolson", "unicorn!", "Kellie", "Colson", "675 Kennedy Ave", "12345"),
@@ -123,9 +122,13 @@ INSERT INTO Travel_Survey Values
 (2, 3, '0', 'Adventurous', 'Family-friendly'),
 (2, 4, '1', 'Traveling with a companion', 'Traveling alone'),
 (2, 5, '1', 'Relaxing', 'Fast-paced');
-=======
+
 -- Feedback_Survey(survey_id, destination_city, destination_country, rating, review)                                
 INSERT INTO Feedback_Survey VALUES (1, "Paris", "France", 4.5, "Paris had so many sights to see, I wish I had more time to explore the city!"),
 								   (2, "Kerry", "Ireland", 4.0, "Kerry was so beautiful!"),
                                    (3, "Sydney", "Australia", 5.0, "Best vacation ever!");
->>>>>>> 9b9deb80ebc5539a22da3704cc1c616c7033be7d
+                                   
+-- Feedback_Survey_User(username, survey_id)
+INSERT INTO Feedback_Survey_User VALUES ("kcolson", 1),
+										("gpautz", 3),
+                                        ("sbowers", 2);
