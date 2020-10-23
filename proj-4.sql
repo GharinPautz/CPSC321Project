32ec2c3f63fdb601487db7ab78b2bdee3db18928
kcolson8 <kelliepanda@gmail.com>
2020-10-22 22:09:43 -0700

    added feedback survey data

--- proj-4.sql
+++ proj-4.sql
@@ -117,3 +117,8 @@ INSERT INTO Destinations VALUES ("Bali", "Indonesia", true, true, true, false, t
                                 ("Marrakesh", "Morocco", false, false, false, true, false, false, true, true, 4.1),
                                 ("Sydney", "Australia", true, false, true, false, true, true, false, true, 4.3),
                                 ("Paris", "France", false, false, true, true, false, false, false, false, 3.9);
+
+-- Feedback_Survey(survey_id, destination_city, destination_country, rating, review)                                
+INSERT INTO Feedback_Survey VALUES (1, "Paris", "France", 4.5, "Paris had so many sights to see, I wish I had more time to explore the city!"),
+								   (2, "Kerry", "Ireland", 4.0, "Kerry was so beautiful!"),
+                                   (3, "Sydney", "Australia", 5.0, "Best vacation ever!");
