c2df7310d75ab03df7115a3b7daf2ff5cd463c43
Gharin Pautz <gharinpautz@gmail.com>
2020-10-22 22:24:52 -0700

    travel survey user inserts

--- proj-4.sql
+++ proj-4.sql
@@ -105,7 +105,7 @@ INSERT INTO Destinations VALUES ("Bali", "Indonesia", true, true, true, false, t
                                 ("Sydney", "Australia", true, false, true, false, true, true, false, true, 4.3),
                                 ("Paris", "France", false, false, true, true, false, false, false, false, 3.9);
 
-<<<<<<< HEAD
+-- User(username, password, name, city, state, address, country, zipcode)
 INSERT INTO User Values 
 ("gpautz", "rainbow!", "Ghar", "Pautz", "123 Ermina Ave", "12345"),
 ("kcolson", "unicorn!", "Kellie", "Colson", "675 Kennedy Ave", "12345"),
@@ -122,10 +122,20 @@ INSERT INTO Travel_Survey Values
 (2, 2, '0', 'Historic', 'Modern'),
 (2, 3, '0', 'Adventurous', 'Family-friendly'),
 (2, 4, '1', 'Traveling with a companion', 'Traveling alone'),
-(2, 5, '1', 'Relaxing', 'Fast-paced');
-=======
+(2, 5, '1', 'Relaxing', 'Fast-paced'),
+(3, 1, '1', 'Beaches', 'Mountains'),
+(3, 2, '1', 'Historic', 'Modern'),
+(3, 3, '0', 'Adventurous', 'Family-friendly'),
+(3, 4, '1', 'Traveling with a companion', 'Traveling alone'),
+(3, 5, '0', 'Relaxing', 'Fast-paced');
+
 -- Feedback_Survey(survey_id, destination_city, destination_country, rating, review)                                
 INSERT INTO Feedback_Survey VALUES (1, "Paris", "France", 4.5, "Paris had so many sights to see, I wish I had more time to explore the city!"),
 								   (2, "Kerry", "Ireland", 4.0, "Kerry was so beautiful!"),
                                    (3, "Sydney", "Australia", 5.0, "Best vacation ever!");
->>>>>>> 9b9deb80ebc5539a22da3704cc1c616c7033be7d
+
+-- Travel_Survey_User(username, survey_id)
+INSERT INTO Travel_Survey_User VALUES
+('gpautz', 1),
+('kcolson', 2),
+('sbowers', 3);
